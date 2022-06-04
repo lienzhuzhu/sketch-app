388b28967d72a0039d04338630bc4485ba6c4fed
Lien Zhu <lienzhucs@gmail.com>
2022-06-03 21:16:03 -0500

    made grid

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // implemetation for etch-a-sketch web app
 //
+
+const grid = document.getElementById("grid")
+
+grid.style.gridTemplateColumns = 'repeat(16, 1fr)';
+grid.style.gridTemplateRows = 'repeat(16, 1fr)';
+
+
+for (let i = 0; i < 16 * 16; i++) {
+    let square = document.createElement("div");
+    square.classList.add("grid-square");
+    grid.appendChild(square);
+}
